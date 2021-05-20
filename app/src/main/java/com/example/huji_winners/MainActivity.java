cb9beda1fba8e919a3b6910e3350465abf323450
Tal Beradt <tal.beradt@mail.huji.ac.il>
2021-05-20 19:05:45 +0300

    register

--- app/src/main/java/com/example/huji_winners/MainActivity.java
+++ app/src/main/java/com/example/huji_winners/MainActivity.java
@@ -4,10 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-
-import com.example.huji_winners.ui.login.LoginActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -23,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openLogAct() {
-        Intent intent = new Intent(this, LoginActivity.class);
+        Intent intent = new Intent(this, signUpActivity.class);
         startActivity(intent);
     }
 }
