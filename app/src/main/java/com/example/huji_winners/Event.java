ac4b185c49f630e4940082c102d89b4656642b91
orrej <orrejwan>
2021-05-20 23:47:09 +0300

    Or

--- app/src/main/java/com/example/huji_winners/Event.java
+++ app/src/main/java/com/example/huji_winners/Event.java
@@ -1,5 +1,7 @@
 package com.example.huji_winners;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Event {
@@ -7,6 +9,8 @@ public class Event {
 	private String eventName;
 
 	private String time;
+	private String date;
+	private String place;
 
 	private final User host;
 
@@ -14,12 +18,14 @@ public class Event {
 
 	private String description;
 
-	public Event(String eventName, String time, User host, String description){
+	public Event(String eventName, String date, String time, User host, String description, String place){
 		this.eventName = eventName;
 		this.time = time;
 		this.host = host;
-		this.attended.add(host);
+//		this.attended.add(host);
 		this.description = description;
+		this.place = place;
+		this.date = date;
 	}
 
 	public void addAttendant(User newAtt){
@@ -34,11 +40,52 @@ public class Event {
 		return this.time;
 	}
 
+	public String getDate(){
+		return this.date;
+	}
+
+	public String getName(){
+		return this.eventName;
+	}
+
 	public User getHost(){
 		return this.host;
 	}
 
 	public String getDescription(){
 		return this.description;
+	}
+
+	public static ArrayList<Event> createEventsList(){
+		ArrayList<Event> lst = new ArrayList<Event>();
+		String[] s = {"12","09","1995"};
+		User user = new User("Or", s, "Nothing", "Vegi", "orrejwan");
+		Event e1 = new Event("Picnic", "12/05/21", "10:30", user, "Fun", "Jerusalem");
+		Event e2 = new Event("Lunch", "20/05/21", "12:30", user, "Fun", "Jerusalem");
+		Event e3 = new Event("a", "20/05/21", "12:30", user, "Fun", "Jerusalem");
+		Event e4 = new Event("s", "20/05/21", "12:30", user, "Fun", "Jerusalem");
+		Event e5 = new Event("d", "20/05/21", "12:30", user, "Fun", "Jerusalem");
+		Event e6 = new Event("f", "20/05/21", "12:30", user, "Fun", "Jerusalem");
+		Event e7 = new Event("g", "20/05/21", "12:30", user, "Fun", "Jerusalem");
+		Event e8 = new Event("h", "20/05/21", "12:30", user, "Fun", "Jerusalem");
+		Event e9 = new Event("i", "20/05/21", "12:30", user, "Fun", "Jerusalem");
+		Event e10 = new Event("j", "20/05/21", "12:30", user, "Fun", "Jerusalem");
+		Event e11 = new Event("k", "20/05/21", "12:30", user, "Fun", "Jerusalem");
+		Event e12 = new Event("l", "20/05/21", "12:30", user, "Fun", "Jerusalem");
+		Event e13 = new Event("m", "20/05/21", "12:30", user, "Fun", "Jerusalem");
+		lst.add(e1);
+		lst.add(e2);
+		lst.add(e3);
+		lst.add(e4);
+		lst.add(e5);
+		lst.add(e6);
+		lst.add(e7);
+		lst.add(e8);
+		lst.add(e9);
+		lst.add(e10);
+		lst.add(e11);
+		lst.add(e12);
+		lst.add(e13);
+		return lst;
 	}
 }
---
+++ app/src/main/java/com/example/huji_winners/eventChoice.java
@@ -0,0 +1,47 @@
+package com.example.huji_winners;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+public class eventChoice extends AppCompatActivity {
+
+	ArrayList<Event> events;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// ...
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_event_choice);
+
+		// Lookup the recyclerview in activity layout
+		RecyclerView rvContacts = (RecyclerView) findViewById(R.id.my_list);
+
+		// Initialize contacts
+		events = Event.createEventsList();
+		// Create adapter passing in the sample user data
+		EventAdapter adapter = new EventAdapter(events);
+		// Attach the adapter to the recyclerview to populate items
+		rvContacts.setAdapter(adapter);
+		// Set layout manager to position the items
+		rvContacts.setLayoutManager(new LinearLayoutManager(this));
+		// That's all!
+
+		FloatingActionButton fab = findViewById(R.id.floatingActionButton);
+		fab.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				startActivity(new Intent(getApplicationContext(), eventChoice.class)); // to Idan's page
+				// to take from the cloud!
+			}
+		});
+	}
+}
