deadd63c3d344606bdaae712024aad734943cb23
Tal Beradt <tal.beradt@mail.huji.ac.il>
2021-05-20 15:57:13 +0300

    firebase jg

--- app/src/main/java/com/example/huji_winners/MainActivity.java
+++ app/src/main/java/com/example/huji_winners/MainActivity.java
@@ -2,15 +2,32 @@ package com.example.huji_winners;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.huji_winners.ui.login.LoginActivity;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button lgnBtn = findViewById(R.id.lgnButton);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // ah thanos
-        //ah getz
+        lgnBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLogAct();
+            }
+        });
+    }
+
+    public void openLogAct() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
