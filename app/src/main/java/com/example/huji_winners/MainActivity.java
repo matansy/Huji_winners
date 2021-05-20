8040027bb2b0023c6e57bcc4a0aa2c0369a6c901
Tal Beradt <tal.beradt@mail.huji.ac.il>
2021-05-20 23:24:48 +0300

    register

--- app/src/main/java/com/example/huji_winners/MainActivity.java
+++ app/src/main/java/com/example/huji_winners/MainActivity.java
@@ -4,17 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseAuth fAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button signUpBtn = findViewById(R.id.signUpButton);
         Button loginBtn = findViewById(R.id.loginButton);
+        Button logoutBtn = findViewById(R.id.logutButton);
+        fAuth = FirebaseAuth.getInstance();
+
+        if (fAuth.getCurrentUser() != null) {
+            signUpBtn.setVisibility(View.INVISIBLE);
+            loginBtn.setVisibility(View.INVISIBLE);
+            logoutBtn.setVisibility(View.VISIBLE);
+        } else {
+            signUpBtn.setVisibility(View.VISIBLE);
+            loginBtn.setVisibility(View.VISIBLE);
+            logoutBtn.setVisibility(View.INVISIBLE);
+        }
+
+
         signUpBtn.setOnClickListener(v -> openSignUpAct());
         loginBtn.setOnClickListener(v -> openLoginAct());
     }
@@ -26,5 +47,10 @@ public class MainActivity extends AppCompatActivity {
     public void openLoginAct() {
         Intent intent = new Intent(this, loginActivity.class);
         startActivity(intent);
+    }
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        finish();
     }
 }
--- app/src/main/java/com/example/huji_winners/signUpActivity.java
+++ app/src/main/java/com/example/huji_winners/signUpActivity.java
@@ -6,27 +6,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class signUpActivity extends AppCompatActivity {
 
     EditText fullName, email, password, dateBirth, diet, describe;
+    RadioGroup radioGrp;
+    RadioButton radioBtn;
     Button registerButton;
     FirebaseAuth fAuth;
     ProgressBar progressBar;
+    FirebaseFirestore fStore;
+    String userID, genderString = "None";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,31 +47,33 @@ public class signUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        radioGrp = (RadioGroup) findViewById(R.id.radioGroup);
         fullName = findViewById(R.id.fullNameText);
-        email = findViewById(R.id.emailText);
-        password = findViewById(R.id.passwordText);
+        email = findViewById(R.id.emailTextLogin);
+        password = findViewById(R.id.passwordTextLogin);
         dateBirth = findViewById(R.id.dateText);
         diet = findViewById(R.id.dietText);
         describe = findViewById(R.id.describeText);
 
         registerButton = findViewById(R.id.registerBtn);
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
         progressBar = findViewById(R.id.progressBar);
 
-//        if (fAuth.getCurrentUser() != null) {
-//            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-//            finish();
-//        }
 
-//        public void logout(View view) {
-//            FirebaseAuth.getInstance().signOut();
-//            startActivity(new Intent(getApplicationContext(), loginActivity.class));
-//            finish();
-//        }
+
+
 
         registerButton.setOnClickListener(v -> {
             String emailString = email.getText().toString().trim();
             String passwordString = password.getText().toString().trim();
+            String fullNameString = fullName.getText().toString().trim();
+            String dateString = dateBirth.getText().toString().trim();
+            String dietString = diet.getText().toString().trim();
+            String describeString = describe.getText().toString().trim();
+
+
+
             // TODO: Check mail ending
             if (TextUtils.isEmpty(emailString)) {
                 email.setError("Invalid Email.");
@@ -75,21 +90,39 @@ public class signUpActivity extends AppCompatActivity {
                 progressBar.setVisibility(View.INVISIBLE);
                 return;
             }
-            // TODO: Handle all data!!
+
             progressBar.setVisibility(View.VISIBLE);
-            fAuth.createUserWithEmailAndPassword(emailString, passwordString).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
-                    if(task.isSuccessful()) {
-                        Toast.makeText(signUpActivity.this, "User Created.", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                        // change to desired page
-                    } else {
-                        Toast.makeText(signUpActivity.this, "User creation failed. "
-                                + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
-                    }
+            fAuth.createUserWithEmailAndPassword(emailString, passwordString).addOnCompleteListener(task -> {
+                if(task.isSuccessful()) {
+                    Toast.makeText(signUpActivity.this, "User Created.", Toast.LENGTH_SHORT).show();
+                    userID = fAuth.getCurrentUser().getUid();
+                    DocumentReference docRef = fStore.collection("users").document(userID);
+                    Map<String, Object> user = new HashMap<>();
+                    user.put("fName", fullNameString);
+                    user.put("email", emailString);
+                    user.put("dateBirth", dateString);
+                    user.put("diet", dietString);
+                    user.put("describe", describeString);
+//                    user.put("sex", genderString);
+                    docRef.set(user).addOnSuccessListener(unused -> Log.d("TAG", "onSuccess: user profile created for " + userID));
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                } else {
+                    Toast.makeText(signUpActivity.this, "User creation failed. "
+                            + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.INVISIBLE);
                 }
             });
         });
+//
+//        radioGrp.setOnCheckedChangeListener((group, checkedId) -> {
+//            switch(checkedId){
+//                case R.id.maleBtn:
+//                    genderString = "Male";
+//                    break;
+//                case R.id.femaleBtn:
+//                    genderString = "Female";
+//                    break;
+//            }
+//        });
     }
 }
