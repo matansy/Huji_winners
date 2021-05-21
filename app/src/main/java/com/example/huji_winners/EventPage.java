da56f6d5ec707781a05266d72cf8edbf85519173
idanrejwan <idanrejwan@gmail.com>
2021-05-21 03:17:00 +0300

    Idan

--- app/src/main/java/com/example/huji_winners/EventPage.java
+++ app/src/main/java/com/example/huji_winners/EventPage.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CreateEvent extends AppCompatActivity {
+public class EventPage extends AppCompatActivity {
 
-    //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_event);
+        setContentView(R.layout.activity_event_page);
     }
 }
